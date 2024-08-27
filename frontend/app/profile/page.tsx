c679a83014a7c17c3719c74ecf5040d9ae984ca7
nikolay <alexandroff.nikolay05@gmail.com>
2024-08-27 18:11:54 +0100

    wip: add user profile layout

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,25 @@
+import Chat from "../../components/Chat";
+
+function UserProfile() {
+  return (
+    <div>
+      <h1>this is the user profile</h1>
+    </div>
+  );
+}
+
+export default function Profile({ children }: { children: React.ReactNode }) {
+  // i need 2 columns, each taking up half the screen width
+
+  return (
+    <div className="flex">
+      <div className="w-8/12 p-4">
+        <h1>Profile chat</h1>
+        <Chat />
+      </div>
+      <div className="w-4/12 p-4">
+        <UserProfile />
+      </div>
+    </div>
+  );
+}
